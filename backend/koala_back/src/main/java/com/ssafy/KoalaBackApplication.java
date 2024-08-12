ace6daecffa75a17ff9164cc05899c200602d046
LeeSpect <wewqwew153@gmail.com>
2024-08-12 15:54:49 +0900

    Refactor: KoalaBackApplication에 스케쥴링 어노테이션 추가

--- backend/koala_back/src/main/java/com/ssafy/KoalaBackApplication.java
+++ backend/koala_back/src/main/java/com/ssafy/KoalaBackApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 public class KoalaBackApplication {
