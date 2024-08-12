0467e817caeac9984ce2a6d053001ac7794d3148
LeeSpect <wewqwew153@gmail.com>
2024-08-12 17:42:36 +0900

    Feat: 스케쥴링으로 매주 월요일마다 주간 공부 시간 업데이트

--- backend/koala_back/src/main/java/com/ssafy/domain/user/model/entity/StudyTimeId.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/user/model/entity/StudyTimeId.java
@@ -7,12 +7,14 @@ import java.util.Objects;
 
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor(access = PROTECTED)
 @Embeddable
