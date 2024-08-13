0ec1c6ef8e6e6a07de2f6b64523f5941edd3009d
LeeSpect <wewqwew153@gmail.com>
2024-08-13 10:27:53 +0900

    Feat: Ranking Repository에 새로운 유저의 순위 반환하는 메서드 추가

--- backend/koala_back/src/main/java/com/ssafy/domain/user/repository/RankingRepository.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/user/repository/RankingRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ssafy.domain.user.model.entity.Ranking;
 
@@ -17,5 +18,9 @@ public interface RankingRepository extends JpaRepository<Ranking, Long> {
 	@Modifying
 	@Query("DELETE FROM Ranking")
 	void clearRankingTable();
+
+	@Query("SELECT COUNT(r) + 1 FROM Ranking r WHERE r.user.userLevel > :userLevel " +
+		"OR (r.user.userLevel = :userLevel AND r.user.userExp > :userExp)")
+	int findNewUserRank(@Param("userLevel") int userLevel, @Param("userExp") int userExp);
 
 }
