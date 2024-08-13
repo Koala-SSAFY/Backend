6d2d8526f98afeff23fbf0d721864a5628a4f284
ssyoung102 <ssyoung102@gmail.com>
2024-08-13 16:35:38 +0900

    Refactor: 실시간 강의 채팅 구현

--- backend/koala_back/src/main/java/com/ssafy/domain/lecture/service/LectureChatService.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/lecture/service/LectureChatService.java
@@ -1,8 +1,6 @@
 package com.ssafy.domain.lecture.service;
 
-import com.ssafy.domain.lecture.chat.LectureChatRoom;
 import com.ssafy.domain.lecture.model.dto.request.LectureChatRequest;
-import com.ssafy.domain.lecture.model.dto.response.LectureChatResponse;
 
 public interface LectureChatService {
 
