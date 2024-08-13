02a478350ed120486307b7cb29cb9a7006d1b76b
LeeSpect <wewqwew153@gmail.com>
2024-08-13 10:18:58 +0900

    Feat: RankingSchedulser Service 구현

--- backend/koala_back/src/main/java/com/ssafy/domain/user/repository/RankingRepository.java
+++ backend/koala_back/src/main/java/com/ssafy/domain/user/repository/RankingRepository.java
@@ -3,6 +3,8 @@ package com.ssafy.domain.user.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ssafy.domain.user.model.entity.Ranking;
 
@@ -11,5 +13,9 @@ public interface RankingRepository extends JpaRepository<Ranking, Long> {
 	Ranking findByUserId(Long userId);
 
 	List<Ranking> findTop10ByOrderByRanking();
+
+	@Modifying
+	@Query("DELETE FROM Ranking")
+	void clearRankingTable();
 
 }
